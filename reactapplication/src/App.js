59ba619db5911189869db83417a5d66bb6327d55
Arash Bazrafshan <abazrafshan123@gmail.com>
2020-06-22 22:42:13 -0500

    Added and styled search bar

--- reactapplication/src/App.js
+++ reactapplication/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Heading from './components/Heading';
+import SearchForm from './components/SearchForm';
 
 function App() {
   return (
     <div className="">
-      <Heading/>
+      <Heading/><br></br>
+
+      <SearchForm/>
     </div>
   );
 }
--- reactapplication/src/components/SearchForm/index.js
+++ reactapplication/src/components/SearchForm/index.js
@@ -1,2 +1,22 @@
 import React, { Component } from "react";
 import "./style.css";
+
+function SearchForm(props) {
+    return (
+        <form className="inputcenter">
+            <div className="searchform">
+                <input 
+                value = {props.search}
+                onChange={props.handleInputChange}
+                name="employeename"
+                type="text"
+                className="form-control"
+                placeholder="Search"
+                id="employeename"
+                />
+            </div>
+        </form>
+    );
+}
+
+export default SearchForm;
